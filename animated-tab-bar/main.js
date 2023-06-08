013f257e0063f877a8bba34117ba248305672e3a
xNoJustice <harunmimarsinan@gmail.com>
2023-06-08 23:39:11 +0300

    added animated tab bar

---
+++ animated-tab-bar/main.js
@@ -0,0 +1,42 @@
+const bgColorsBody = ["#ffb457", "#ff96bd", "#9999fb", "#ffe797", "#cffff1"];
+const menu = document.body.querySelector(".menu");
+const menuItems = menu.querySelectorAll(".menu-item");
+const menuBorder = menu.querySelector(".menu-border");
+let activeItem = menu.querySelector(".active");
+
+function clickItem(item, index) {
+  menu.style.removeProperty("--timeOut");
+
+  if (activeItem == item) return;
+
+  if (activeItem) {
+    activeItem.classList.remove("active");
+  }
+
+  item.classList.add("active");
+  document.body.style.backgroundColor = bgColorsBody[index];
+  activeItem = item;
+  offsetMenuBorder(activeItem, menuBorder);
+}
+
+function offsetMenuBorder(element, menuBorder) {
+  const offsetActiveItem = element.getBoundingClientRect();
+  const left =
+    Math.floor(
+      offsetActiveItem.left -
+        menu.offsetLeft -
+        (menuBorder.offsetWidth - offsetActiveItem.width) / 2
+    ) + "px";
+  menuBorder.style.transform = `translate3d(${left}, 0 , 0)`;
+}
+
+offsetMenuBorder(activeItem, menuBorder);
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => clickItem(item, index));
+});
+
+window.addEventListener("resize", () => {
+  offsetMenuBorder(activeItem, menuBorder);
+  menu.style.setProperty("--timeOut", "none");
+});
